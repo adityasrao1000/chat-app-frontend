311b2c9957e924ef58e01689a5e12f19f47299b7
Aditya Rao <adityasrao1000@gmail.com>
2018-08-27 17:10:07 +0530

    changes made

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -12,7 +12,7 @@ export class ChatRoomComponent implements AfterViewInit {
 
     userList: string[] = [];
     messages: string[] = [];
-    msg = '';
+    msg: string;
     smily = '&#x1F642';
     online: Observable<boolean>;
     isonline: boolean;
@@ -21,7 +21,10 @@ export class ChatRoomComponent implements AfterViewInit {
     webSocket: WebSocket;
     retry = 1;
     chatroom: string;
+
     constructor(@Inject(DOCUMENT) private document: any, private route: ActivatedRoute) {
+
+        this.msg = '';
         this.route.params.subscribe(params => {
             this.chatroom = params['id'];
         });
@@ -42,43 +45,65 @@ export class ChatRoomComponent implements AfterViewInit {
     }
 
     ngAfterViewInit() {
+        /**
+         * add change event listener to file input after its added to the markups
+         */
         fromEvent(this.document.getElementById('file'), 'change')
             .subscribe(() => {
                 this.webSocket.send(this.document.getElementById('file').files[0]);
             });
     }
 
+    /**
+     * wrapper function that manually clicks on the hidden file input field.
+     */
     fileClick() {
         this.document.getElementById('file').click();
     }
 
-    initializeSocket(): any {
-        console.log('initializing web socket');
-        this.webSocket = new WebSocket(`ws://localhost:4567/${this.chatroom}`);
-        fromEvent(this.webSocket, 'message')
-            .subscribe(msg => this.updateChat(msg));
+    /**
+     * creates a new socket connection and adds event listeners to the socket
+     */
+    initializeSocket() {
+        if ('WebSocket' in window) {
+            console.log('initializing web socket');
+            this.webSocket = new WebSocket(`ws://localhost:4567/${this.chatroom}`);
 
-        fromEvent(this.webSocket, 'close')
-            .subscribe(() => {
-                if (this.isonline) {
-                    if (this.retry <= 3) {
-                        console.log('trying to reconnect, attempt: ' + this.retry);
-                        this.retry++;
-                        this.initializeSocket();
+            /**
+             * event listener fires when the socket recieves a message from the server
+             */
+            fromEvent(this.webSocket, 'message')
+                .subscribe(msg => this.updateChat(msg));
+
+            /**
+             * event listener fires when the socket connection is closed
+             */
+            fromEvent(this.webSocket, 'close')
+                .subscribe(() => {
+                    if (this.isonline) {
+                        if (this.retry <= 3) {
+                            console.log('trying to reconnect, attempt: ' + this.retry);
+                            this.retry++;
+                            this.initializeSocket();
+                        }
                     }
-                }
-            });
+                });
+        } else {
+            window.alert('your browser does not support web sockets');
+        }
     }
 
+    /**
+     *  wrapper function
+     */
     send() {
         this.sendMessage(this.msg);
     }
 
-    message(e) {
-        this.sendMessage(this.msg);
-    }
-
-    // Send a message if it's not empty, then clear the input field
+    /**
+     * Send a message if it's not empty, then clear the input field
+     * @param message
+     */
     sendMessage(message) {
         if (message !== '') {
             this.webSocket.send(message);
@@ -86,11 +111,18 @@ export class ChatRoomComponent implements AfterViewInit {
         }
     }
 
+    /**
+     * scrolls to the bottom of the chat box after image loads
+     */
     imageLoad() {
         this.document.getElementById('scroll').scrollIntoView(true);
     }
 
-    // Update the chat-panel, and the list of connected users
+    /**
+     * Update the chat-panel, and the list of connected users when the socket receives
+     *  a message from the server
+     * @param msg
+    */
     updateChat(msg) {
         if (msg.data instanceof Blob) {
             this.messages.push(window.URL.createObjectURL(msg.data));
