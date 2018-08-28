988d03bfb80db564a14d74e72a20149811e2efa1
Aditya Rao <adityasrao1000@gmail.com>
2018-08-28 11:32:12 +0530

    emojis added

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, AfterViewInit } from '@angular/core';
+import { Component, Inject, AfterViewInit, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 import { fromEvent, Observable, of, merge } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
     templateUrl: './chatroom.component.html',
     styleUrls: ['./chatroom.component.scss']
 })
-export class ChatRoomComponent implements AfterViewInit {
+export class ChatRoomComponent implements OnInit, AfterViewInit {
 
     userList: string[] = [];
     messages: string[] = [];
@@ -16,7 +16,8 @@ export class ChatRoomComponent implements AfterViewInit {
     smily = '&#x1F642';
     online: Observable<boolean>;
     isonline: boolean;
-    emojis = ['&#x1F911'];
+    emojis = ['&#x1F911', '&#x1F642', '&#x1F600', '&#x1F601', '&#x1F602', '&#x1F603', '&#x1F604', '&#x1F605', '&#x1F606', '&#x1F607',
+        '&#x1F608', '&#x1F609', '&#x1F60A', '&#x1F60B', '&#x1F60C', '&#x1F60D', '&#x1F60E', '&#x1F60F'];
     webSocket: WebSocket;
     retry = 1;
     chatroom: string;
@@ -26,14 +27,12 @@ export class ChatRoomComponent implements AfterViewInit {
         // initialize variables
         this.unread_msg_count = 0;
         this.msg = '';
+    }
 
+    ngOnInit(): void {
         merge(
             fromEvent(window, 'focus').pipe(mapTo(true))
-        ).subscribe(val => {
-            if (val) {
-                this.document.title = this.unread_msg_count = 0;
-            }
-        });
+        ).subscribe(() => this.document.title = this.unread_msg_count = 0);
 
         this.route.paramMap.subscribe(params => {
             this.chatroom = params.get('id');
@@ -52,8 +51,8 @@ export class ChatRoomComponent implements AfterViewInit {
                 this.isonline = false;
             }
         });
-    }
 
+    }
     ngAfterViewInit() {
         /**
          * add change event listener to file input after its added to the markups
@@ -71,6 +70,13 @@ export class ChatRoomComponent implements AfterViewInit {
         this.document.getElementById('file').click();
     }
 
+    /**
+     * appends an enoji to the message string
+     * @param emoji
+     */
+    addEmoji(emoji) {
+        this.msg += emoji;
+    }
     /**
      * creates a new socket connection and adds event listeners to the socket
      */
